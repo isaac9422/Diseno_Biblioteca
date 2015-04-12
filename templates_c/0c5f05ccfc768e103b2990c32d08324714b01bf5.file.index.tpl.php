a61ca645e9b3cc02cda4c6b09a9c36516ff85421
Jhunior <isaac9422@gmail.com>
2015-04-12 17:47:53 -0500

    Estado inicial con glight y gitignore

---
+++ templates_c/0c5f05ccfc768e103b2990c32d08324714b01bf5.file.index.tpl.php
@@ -0,0 +1,25 @@
+<?php /* Smarty version Smarty-3.0.9, created on 2015-03-05 17:04:25
+         compiled from "/opt/lampp/htdocs/glight/templates/index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:180964388754f87e8997a545-58204834%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '0c5f05ccfc768e103b2990c32d08324714b01bf5' => 
+    array (
+      0 => '/opt/lampp/htdocs/glight/templates/index.tpl',
+      1 => 1354557782,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '180964388754f87e8997a545-58204834',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+<div class="square">
+<table cellspacing="5">
+<tr><td><h4>Framework G</h4>
+G is a framework and an architectural pattern that is based on the MVC (model-view-controller) but it makes it better, integrates new layers and redefine some of its layers, the base of G is the object-oriented programming OOP and class diagram. In base on these, defines a CRUD and installer modules that facilitate the development to the developer and save you time and work. 
+</td></tr></table>
+</div>
---
+++ templates_c/dd99f4b1fa9add3dd4d01881621784dbf40d3909.file.footer.tpl.php
@@ -0,0 +1,48 @@
+<?php /* Smarty version Smarty-3.0.9, created on 2015-03-05 17:04:25
+         compiled from "/opt/lampp/htdocs/glight/templates/footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:183947548254f87e89982400-27932460%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'dd99f4b1fa9add3dd4d01881621784dbf40d3909' => 
+    array (
+      0 => '/opt/lampp/htdocs/glight/templates/footer.tpl',
+      1 => 1360268968,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '183947548254f87e89982400-27932460',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+</td></tr></table>
+
+</div>
+<!-- End Content -->
+</div>
+<!-- End Container -->
+
+<!-- Begin Footer -->
+<footer>
+<div id="footer">  
+<div class="container">
+<table cellspacing="0" cellpadding="0">
+<tr><td align="left" class="footerstyle">
+<a href="http://www.facebook.com/frameworkg" target="_blank"><img src="<?php echo $_smarty_tpl->getVariable('gvar')->value['l_global'];?>
+images/fb.png" /></a>&nbsp;<a href="http://twitter.com/frameworkg" target="_blank"><img src="<?php echo $_smarty_tpl->getVariable('gvar')->value['l_global'];?>
+images/twitter.png" /></a>&nbsp;<img src="<?php echo $_smarty_tpl->getVariable('gvar')->value['l_global'];?>
+images/flickr.png" />
+</td><td align="right" class="footerstyle"><!-- Don't remove copyright -->Based in: <a href="http://www.frameworkg.com" style="color:#FFF;" target="_blank">Framework G</a><br />All Rights Reserved &copy; 2011 - <?php echo date('Y');?>
+<!-- Don't remove copyright -->
+</td></tr>
+</table>
+</div>
+</div>
+</footer>
+<!-- End Footer -->
+
+</div>
+</body>
+</html>
