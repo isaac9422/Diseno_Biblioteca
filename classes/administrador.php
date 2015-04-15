26c4bb567e931f337ffc86828af5504a17e5849d
Jhunior <isaac9422@gmail.com>
2015-04-15 10:38:50 -0500

    Clase administrador

---
+++ classes/administrador.php
@@ -0,0 +1,40 @@
+<?php
+ 
+class administrador extends object_standard
+{
+	//attributes
+	protected $identificacion;
+	protected $nombre;
+	protected $contraseña;
+	protected $direccion;
+        protected $telefono;
+	protected $email;
+	
+	//components
+	var $components = array();
+	
+	//auxiliars for primary key and for files
+	var $auxiliars = array();
+	
+	//data about the attributes
+	public function metadata()
+	{
+		return array("identificacion" => array(), "nombre" => array(), "contraseña" => array(), "direccion" => array(), "telefono" => array(), "email" => array()); 
+	}
+
+	public function primary_key()
+	{
+		return array("identificacion");
+	}
+	
+	public function relational_keys($class, $rel_name)
+	{
+		switch($class)
+		{		
+		    default:
+			break;
+		}
+	}
+}
+
+?>
